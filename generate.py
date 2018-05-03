4a47601a94546a1bc208565b086aef50b21c448b
Alex Semenov <aleksss123@yandex.ru>
2018-05-03 14:18:41 +0300

    wrote documentary + minor fixes

--- generate.py
+++ generate.py
@@ -1,11 +1,80 @@
+"""Program for generating the text using the model.
+Has some options; more info in --help."""
+
 import string
 import numpy as np
 import argparse
 import sys
 import os
 
+
+def prepare_args():
+    """
+Works with called args. Returns the first word of generated text.
+    """
+    global output_file
+    if args.output:
+        output_file = open(args.output, 'w')
+    else:
+        output_file = sys.stdout
+
+    first = args.seed
+
+    model_dir = os.getcwd()
+    if args.model == "\\model":
+        model_dir += "\\model"
+    else:
+        model_dir = args.model
+
+    os.chdir(model_dir)
+    return first
+
+
+def generate_random(begin=""):
+    """
+The most important function. Takes a word, returns generated word.
+If it's not possible to generate a word from taken word, returns
+random word from "index"-file.
+    """
+    if begin is None or begin == "" or begin not in index_dict:
+        out = np.random.choice(list(index_dict.keys()), 1)[0]
+    else:
+        file_number = index_dict[begin]
+        with open("list"+str(file_number)+".rtf") as curr_file:
+            """
+Parses a string with taken word, it is located in some "list"-file.
+            """
+            for line in curr_file:
+                pair = [x for x in line.split(':')]
+                if pair[0] == begin:
+                    cont_words = pair[1].split('#')
+                    """
+I use 2 arrays: choice_arr for all possible words for output,
+frequency - for their frequences.
+Variable "summ" is a sum of all frequences.
+                    """
+                    frequency = []
+                    choice_arr = []
+                    summ = 0
+                    for pair_word_freq in cont_words:
+                        if pair_word_freq != '':
+                            pair = [x for x in pair_word_freq.split()]
+                            succ_word = pair[0]
+                            choice_arr.append(succ_word)
+                            frequency.append(float(pair[1]))
+                            summ += int(pair[1])
+                    """
+Normalize the frequences, then generate a random word from choice_arr.
+                    """
+                    for i in range(len(frequency)):
+                        frequency[i] = frequency[i]/summ
+                    out = np.random.choice(choice_arr, None, True, frequency)
+
+    return out
+
+
 if __name__ == "__main__":
-    
+
     parser = argparse.ArgumentParser(description='A generator program.')
     parser.add_argument("-m", "--model", default="\\model",
                         help="Input model path.")
@@ -18,61 +87,24 @@ if __name__ == "__main__":
                         Standart output by default.""")
     args = parser.parse_args()
 
-    if args.output:
-        output_file = open(args.output, 'w')
-    else:
-        output_file = sys.stdout
+    word = prepare_args()
 
-    link = args.seed
-
-    model_dir = os.getcwd()
-    if args.model == "\\model":
-        model_dir += "\model"
-    else:
-        model_dir = args.model
-
-    os.chdir(model_dir)
-
+    """
+Saves into dict our "index"-file for quicker access.
+    """
     index_dict = dict()
     with open("index.rtf") as index_file:
         for line in index_file:
             pair = [x for x in line.split()]
             index_dict[pair[0]] = int(pair[1])
 
-
-def generate_random(begin=""):
-    if begin is None or begin == "":
-        word = np.random.choice(list(index_dict.keys()), 1)[0]
-    else:
-        file_number = index_dict[begin]
-        with open("list"+str(file_number)+".rtf") as curr_file:
-            for line in curr_file:
-                pair = [x for x in line.split(':')]
-                if pair[0] == begin:
-                    words = pair[1].split('#')
-                    frequency = []
-                    choice_arr = []
-                    summ = 0
-                    for w in words:
-                        if w != '':
-                            lst2 = [x for x in w.split()]
-                            succ_word = lst2[0]
-                            choice_arr.append(succ_word)
-                            frequency.append(float(lst2[1]))
-                            summ += int(lst2[1])
-                    for i in range(len(frequency)):
-                        frequency[i] = frequency[i]/summ
-                    word = np.random.choice(choice_arr, None, True, frequency)
-
-    return word
-
-
-if __name__ == "__main__":
-
+    """
+Generator cycle. Each step generates one word.
+    """
     for i in range(args.length):
-        if link is not None:
-            output_file.write(link + ' ')
-        link = generate_random(link)
+        if word is not None:
+            output_file.write(word + ' ')
+        word = generate_random(word)
 
     if output_file is not sys.stdout:
         output_file.close()
